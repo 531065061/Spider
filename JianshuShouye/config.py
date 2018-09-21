107a791c70e8018644c6c8eed83c5cd6c440e88c
SeIeAt <531065061@qq.com>
2018-09-21 14:56:02 +0800

    123

---
+++ JianshuShouye/config.py
@@ -0,0 +1,3 @@
+MONGO_URL='192.168.66.129:27017'
+MONGO_DB='JianshuShouye'
+MONGO_TABLE='jianshuShouye'
--- JianshuShouye/spider.py
+++ JianshuShouye/spider.py
@@ -1,44 +1,67 @@
 import requests
 from lxml import etree
+import time
+import pymongo
+from config import *
 
-base_url='http://www.jianshu.com/c/bDHhpK'
-headers={
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+base_url = 'http://www.jianshu.com/c/bDHhpK'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
 }
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
+
+
 def get_html(offset):
-    queries='?order_by=commented_at&page='+str(offset)
-    url=base_url+queries
-    print(url)
-    response=requests.get(url,headers=headers)
-    if response.status_code==200:
+    queries = '?order_by=commented_at&page=' + str(offset)
+    url = base_url + queries
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
         return response
     return None
 
+
 def parse_html(html):
-    selector=etree.HTML(html.text)
+    selector = etree.HTML(html.text)
     infos = selector.xpath('//ul[@class="note-list"]/li')
     for info in infos:
-        auth=info.xpath('div/a/text()')[0]
-        comment=info.xpath('div/p/text()')[0]
-        nockname=info.xpath('div/div/a[1]/text()')[0]
-        iconfont=info.xpath('div/div/a[2]/text()')[1]
-        like=info.xpath('div/div/span[1]/text()')[0]
-        moneys=info.xpath('div/div/span[2]/text()')
-        if len(moneys)==0:
-            money='0'
+        url = info.xpath('div/a/@href')[0]
+        auth = info.xpath('div/a/text()')[0].strip()
+        comment = info.xpath('div/p/text()')[0].strip()
+        nockname = info.xpath('div/div/a[1]/text()')[0].strip()
+        iconfont = info.xpath('div/div/a[2]/text()')[1].strip()
+        like = info.xpath('div/div/span[1]/text()')[0].strip()
+        moneys = info.xpath('div/div/span[2]/text()')
+        if len(moneys) == 0:
+            money = '无'
         else:
-            money=moneys[0]
-        date={
-            'auth':auth,
+            money = moneys[0]
 
+        date = {
+            'url': 'http://www.jianshu.com' + url,
+            'auth': auth,
+            'comment': comment,
+            'nockname': nockname,
+            'iconfont': iconfont,
+            'like': like,
+            'money': money
         }
-        print(auth,comment,nockname,iconfont,like,money)
+        save_parse(date)
 
-def main():
-    html=get_html(2)
-    print(html)
+
+def save_parse(result):
+    if db[MONGO_TABLE].insert_one(result):
+        print('存储到MongDB成功', result)
+        return True
+    return False
+
+
+def main(offset):
+    html = get_html(offset)
     parse_html(html)
+    time.sleep(2)
+
 
 if __name__ == '__main__':
-    main()
-
+    for offset in range(1, 10):
+        main(offset)
